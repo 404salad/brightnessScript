3e85c9d78a103277cd3a60dafe656416ef731f5d
404salad <404salad@gmail.com>
2023-09-23 09:42:48 +0530

    added support for relative brightness +5 -5 etc

--- brightness2.sh
+++ brightness2.sh
@@ -7,12 +7,47 @@ fi
 
 percentage="$1"
 
-if [ "$percentage" -ge 1 ] && [ "$percentage" -le 100 ]; then
-    max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-    brightness=$((percentage * max_brightness / 100))
+if [[ "$percentage" == +* ]]; then
+    # Increase brightness
+    percentage="${percentage#+}"
+    if [ "$percentage" -ge 1 ] && [ "$percentage" -le 100 ]; then
+        max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+        current_brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+        new_brightness=$((current_brightness + (percentage * max_brightness / 100)))
 
-    sudo bash -c "echo '$brightness' > /sys/class/backlight/intel_backlight/brightness"
+        if [ "$new_brightness" -le "$max_brightness" ]; then
+            sudo bash -c "echo '$new_brightness' > /sys/class/backlight/intel_backlight/brightness"
+        else
+            echo "Brightness cannot exceed the maximum value."
+        fi
+    else
+        echo "Invalid argument. Brightness percentage should be between 0 and 100."
+    fi
+elif [[ "$percentage" == -* ]]; then
+    # Decrease brightness
+    percentage="${percentage#-}"
+    if [ "$percentage" -ge 1 ] && [ "$percentage" -le 100 ]; then
+        max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+        current_brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
+        new_brightness=$((current_brightness - (percentage * max_brightness / 100)))
+
+        if [ "$new_brightness" -ge 0 ]; then
+            sudo bash -c "echo '$new_brightness' > /sys/class/backlight/intel_backlight/brightness"
+        else
+            echo "Brightness cannot be less than 0."
+        fi
+    else
+        echo "Invalid argument. Brightness percentage should be between 0 and 100."
+    fi
 else
-    echo "Invalid argument. Brightness percentage should be between 0 and 100."
+    # Set brightness to the specified percentage
+    if [ "$percentage" -ge 1 ] && [ "$percentage" -le 100 ]; then
+        max_brightness=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+        brightness=$((percentage * max_brightness / 100))
+
+        sudo bash -c "echo '$brightness' > /sys/class/backlight/intel_backlight/brightness"
+    else
+        echo "Invalid argument. Brightness percentage should be between 0 and 100."
+    fi
 fi
 
